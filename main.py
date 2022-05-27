63645ba4e54967789c80f911f671f541efd28016
RybOlya <olga9rybenchuk@gmail.com>
2022-05-27 09:10:39 +0300

    Modifying class

--- main.py
+++ main.py
@@ -8,14 +8,12 @@ from models.university import University
 
 
 def main():
-    martha_batkovets = Student("Martha", "Batkovets","Volodymyrivna","NULP", "IKTA",EducationLevel.BACHELOR,"IoT", FormOfEducation.FULL_TIME,"IR",14,2023,4.4)
-    oleh_delych = Student("Oleh", "Delych","Maksymovuch","NULP", "IKTA",EducationLevel.BACHELOR,"IoT", FormOfEducation.FULL_TIME,"IR",14,2023,3.4)
- 
+    martha_batkovets = Student("Martha", "Batkovets","Volodymyrivna", EducationLevel.BACHELOR, FormOfEducation.FULL_TIME,2023,4.4)
+    oleh_delych = Student("Oleh", "Delych","Maksymovuch",EducationLevel.BACHELOR, FormOfEducation.FULL_TIME,2023,3.4)
     students_IR = {martha_batkovets,oleh_delych}
     group_IR = Group("NULP", "IKTA","Iot","IR",14,students_IR)
-    print(martha_batkovets)
+    print(group_IR)
     print(martha_batkovets.student_performance())
-    print(oleh_delych)
     print(oleh_delych.student_performance())
     lectures = {"mon":["Сircuitry","Math"],
                 "tue":["Math","Сircuitry","History"],
--- models/student.py
+++ models/student.py
@@ -1,15 +1,13 @@
 from models.education_level import EducationLevel
 from models.form_of_education import FormOfEducation
-from models.group import Group
 from models.performance import Performance
 
 
-class Student(Group):
+class Student():
 
-    def __init__(self, first_name: str, last_name: str, middle_name: str, university_name: str, faculty: str, education_level: EducationLevel, 
-                 major: str, form_of_study: FormOfEducation, group_name: str, group_number: str, graduation_year: str, average_score: float) -> None:
+    def __init__(self, first_name: str, last_name: str, middle_name: str, education_level: EducationLevel, 
+                form_of_study: FormOfEducation,  graduation_year: str, average_score: float) -> None:
 
-        super().__init__(university_name, faculty, major, group_name, group_number,0)
         self._first_name = first_name
         self._last_name = last_name
         self._middle_name = middle_name
